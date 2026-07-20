ae5a09568a51f58f850011f4c91511a69e6649e0
Bella Jin <35979424+JiEun11@users.noreply.github.com>
2026-07-21 01:06:43 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 64.43 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,21 +1,29 @@
 function solution(participant, completion) {
-    const countByParticipant = new Map()
-    
-    // participant 카운팅
-    participant.forEach((participant) => {
-        countByParticipant
-            .set(participant, 
-                (countByParticipant.get(participant) ||                 0) + 1)
-        });
-    
-    // completion에서 카운트 빼기
-    completion.forEach((participant) => {
-        countByParticipant
-            .set(participant,
-                (countByParticipant.get(participant) ||                 0) -1)
-        });
-    
-    // 값이 1인 사람 찾기
-    console.log([...countByParticipant])
-    return [...countByParticipant].find(([name, count]) => count >0)[0];
+  const frequencyMap = new Map();
+
+  // 1단계: participant를 돌면서 맵 채우기 (동명이인 카운트)
+  for (const name of participant) {
+    // 여기: name이 이미 map에 있으면 +1, 없으면 1로 시작
+    // .has() 써서 분기해봐
+    if(frequencyMap.has(name)) {
+        frequencyMap.set(name, frequencyMap.get(name)+1)
+    }
+    else {
+        frequencyMap.set(name, 1);
+    }
+  }
+
+  // 2단계: completion을 돌면서 카운트 차감
+  for (const name of completion) {
+    // 여기: 완주자는 -1
+     if (frequencyMap.has(name)) {
+        frequencyMap.set(name, frequencyMap.get(name)-1)   
+     }
+  }
+
+  // 3단계: 카운트가 1 이상 남은 사람 찾아서 반환
+  for (const [name, count] of frequencyMap) {
+    // 여기
+     if (count >= 1) return name;
+  }
 }
