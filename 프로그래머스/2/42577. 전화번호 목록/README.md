ca5c4c63d5969f77b618b6da801c999e61ac6616
Bella Jin <35979424+JiEun11@users.noreply.github.com>
2026-07-22 17:41:52 +0900

    [level 2] Title: 전화번호 목록, Time: 97.51 ms, Memory: 97.7 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/README.md
+++ 프로그래머스/2/42577. 전화번호 목록/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전화번호 목록 - 42577 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577) 
 
 ### 성능 요약
 
-메모리: 100 MB, 시간: 258.63 ms
+메모리: 97.7 MB, 시간: 97.51 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 01월 21일 01:50:40
+2026년 07월 22일 17:41:49
 
 ### 문제 설명
 
--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,17 +1,13 @@
 function solution(phone_book) {
-    //1. 모든 번호를 Set에 저장
-    const phoneBookSet = new Set(phone_book);
-    let answer = true;
-    // 2. 각 번호의 앞부분(접두어)이 Set에 있는지 확인
-    for (const phone of phone_book) {
-        // phone의 접두어들을 하나씩 확인
-        for (let i = 1; i < phone.length; i++) {
-            const prefix = phone.slice(0, i);
-            
-            if (phoneBookSet.has(prefix)) {
-                return false;
-            }
-        }
+    // 1. 정렬
+    const sortedPhoneBook = [...phone_book].sort();
+    
+    // 2. 인접한 번호들을 순회
+    for (let i = 0; i < sortedPhoneBook.length-1; i++) {
+        // 3. 접두어 관계가 발견되면 false
+        if (sortedPhoneBook[i+1].startsWith(sortedPhoneBook[i])) return false;
     }
-    return answer;
+    
+    // 4. 끝까지 없으면 true
+    return true;
 }
