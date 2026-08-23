f16356520e5a4d9e735ccd3864947d81be978623
Bella Jin <35979424+JiEun11@users.noreply.github.com>
2026-08-23 20:35:59 +0900

    [level 3] Title: 단어 변환, Time: 0.29 ms, Memory: 43.9 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.js
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.js
@@ -1,44 +1,34 @@
-function canTransform(word1, word2) {
-    let differenceCount = 0;
-    for (let i = 0; i < word1.length; i++) {
-        if (word1[i] !== word2[i]) {
-            differenceCount++;
+function isOneStepAway(wordA, wordB) {
+    let diffrentCount = 0;
+    
+    for (let i = 0 ; i < wordA.length; i++) {
+        if (wordA[i] !== wordB[i]) {
+            diffrentCount++;
         }
     }
-    
-    return differenceCount === 1;
+    return diffrentCount === 1;
 }
 
 function solution(begin, target, words) {
-    if (!words.includes(target)) return 0;
-
     const queue = [[begin, 0]];
-    const visited = new Set([begin]);
-    let head = 0;
-
-    while (head < queue.length) {
-        const [currentWord, distance] = queue[head++];
-
+    
+    while (queue.length !==0 ) {
+        const [currentWord, distance] = queue.shift();
+        
         if (currentWord === target) {
             return distance;
         }
-
-        for (const nextWord of words) {
-            // 이미 방문했다면 건너뛰기
-            if (visited.has(nextWord)) {
-                continue;
+        
+        for (const word of [...words]) {
+            if (isOneStepAway(currentWord, word)) {
+                // words에서 제거 + 큐에 push
+                const index = words.indexOf(word);
+                words.splice(index, 1);
+                
+                queue.push([word, distance+1]);
             }
-            // currentWord와 nextWord가 한 글자만 다른지 확인
-            const isDifferenceOneGap = canTransform(currentWord, nextWord);
-            if (isDifferenceOneGap) {
-                // 방문 처리
-                visited.add(nextWord);
-                // 다음 거리와 함께 큐에 넣기
-                queue.push([nextWord, distance+1]);    
-            };
-                        
         }
     }
-
-    return 0;
+    
+    return 0; 
 }
