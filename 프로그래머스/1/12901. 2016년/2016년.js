856bf13501d9c1f2226648eea0b18626e86db245
Bella Jin <35979424+JiEun11@users.noreply.github.com>
2026-09-11 00:13:59 +0900

    [level 1] Title: 2016년, Time: 0.08 ms, Memory: 44.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12901. 2016년/2016년.js
@@ -0,0 +1,13 @@
+function solution(a, b) {
+    const days = ["FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"];
+    const monthDays = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    let totalDays = 0;
+    
+    for (let month = 1; month < a; month++) {
+        totalDays += monthDays[month-1];
+    }
+    totalDays += b;
+    totalDays -= 1;
+    
+    return  days[totalDays % 7];
+}
